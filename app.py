5bb9ec849eac20ab7161a62aa430e0d6af453fcf
alexander-vanhorn <alexander.vanhorn@utp.ac.pa>
2023-10-26 12:44:42 -0500

    Código de software con la gráfica

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
-print("Hello world")
+import dash
+from dash import dcc, html
+import plotly.express as px
+import pandas as pd
+
+# Carga los datos desde el archivo CSV
+data_url = "https://raw.githubusercontent.com/alexander-vanhorn/instrumento-evaluacion/main/datos/Grado%20de%20satisfacci%C3%B3n%20general.csv"
+df = pd.read_csv(data_url)
+
+# Inicializa la aplicación Dash
+app = dash.Dash(__name__)
+
+# Define la disposición de la aplicación
+app.layout = html.Div([
+    html.H1("Distribución de Respuestas"),
+    dcc.Graph(
+        id='pie-chart',
+        figure=px.pie(df, names='MD;A;ND;D;TD;NA', title='Distribución de Respuestas')
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
